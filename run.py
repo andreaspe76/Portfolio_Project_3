5508292dd0260d7363297097a1e10c03163d1658
Andreas Pergantis <andreaspergantis@hotmail.com>
2025-03-11 11:53:17 +0100

    Create ne game function, add welcome message and ask for player's name

--- run.py
+++ run.py
@@ -1,4 +1,11 @@
-#Import randit from random module
+# Import randit from random module
 from random import randint
 
 
+def new_game():
+    print("Welcome to my Battleship game!")
+    player_name = input("What is your name? ")
+    print(f"Hello, {player_name}! Let's play Battleship.")
+
+
+new_game()
